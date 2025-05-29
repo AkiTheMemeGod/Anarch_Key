458a690c7abcef5935e815bb561cbf1dfd4ff6b2
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-05-29 14:00:02 +0530

    Refactor Anarch2FA class to improve email handling and database interactions; add duplicate checks for email and username

--- Anarch2FA.py
+++ Anarch2FA.py
@@ -1,19 +1,44 @@
+import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import random as rd
-
+from dotenv import load_dotenv
+load_dotenv()
+import sqlite3 as sq
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
 
 class Anarch2FA:
+    def __init__(self):
+        self.con = sq.connect('AnarchKey.db')
+        self.cur = self.con.cursor()
+        self.path = os.path.dirname(__file__)
     @staticmethod
     def generate_otp():
         otp = rd.randint(100000, 999999)
         return otp
 
+    def duplicate_email_check(self, email):
+        self.cur.execute("SELECT * FROM USERS WHERE email=?", (email,))
+        result = self.cur.fetchone()
+        if result:
+            return True
+        else:
+            return False
+
+    def duplicate_username_check(self, username):
+        self.cur.execute("SELECT * FROM USERS WHERE username=?", (username,))
+        result = self.cur.fetchone()
+        if result:
+            return True
+        else:
+            return False
+
     def send_otp(self, email, username):
         otp = self.generate_otp()
-        subject = "Your Protobase Signup OTP Code"
-        with open(f'{path}otp.html', 'r') as file:
+        subject = "Your AnarchKey Signup OTP Code"
+        with open(f'{self.path}/static/otp.html', 'r') as file:
             body = file.read()
 
         # Replace placeholders with actual values
@@ -22,7 +47,7 @@ class Anarch2FA:
         sender_password = SENDER_PASSWORD
 
         message = MIMEMultipart()
-        message["From"] = "ProtoBase"
+        message["From"] = "AnarchKey"
         message["To"] = email
         message["Subject"] = subject
         message.attach(MIMEText(body, "html"))
@@ -31,15 +56,16 @@ class Anarch2FA:
             with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
                 server.login(sender_email, sender_password)
                 server.send_message(message)
+            print("Email sent successfully")
             return True, otp
         except Exception as e:
+            print(f"Failed to send email: {e}")
             return False, None
 
-    @staticmethod
-    def send_reset_password_email(email, username, reset_link):
+    def send_reset_password_email(self, email, username, reset_link):
         subject = "Protobase Password Reset Request"
 
-        with open(f'{path}reset_password_template.html', 'r') as file:
+        with open(f'{self.path}reset_password_template.html', 'r') as file:
             body = file.read()
 
         body = body.replace("{username}", username).replace("{reset_link}", reset_link)
